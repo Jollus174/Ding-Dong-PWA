60bc9aa6e1c4609c3f1d72bece62f8bad533a170
JoelS <joelsweetman174@msn.com>
2017-07-19 09:46:42 +0800

    Allowing 'ESC' to deactivate both character and menu box.

--- public/js/custom.js
+++ public/js/custom.js
@@ -317,7 +317,7 @@ $(document).ready(function(){
 		}
 		if (isEscape){
 			// if character is active, deactivate it
-			if($('.character-box.active').length){
+			if($('body').hasClass('no-scroll')){
 				deactivateCharacter();
 			} else {
 				// else toggle the sidemenu instead
