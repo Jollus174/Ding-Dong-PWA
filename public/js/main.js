11325cda2c47f6704aefe9527c1566777869fc3d
JoelS <joelsweetman174@msn.com>
2017-07-25 00:59:59 +0800

    Service Worker should hopefully be working (thank Christ). Still need to be able to push through updates whenever, though.

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 // Register the service worker (if available).
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('./sw2.js').then(function(reg){
+    navigator.serviceWorker.register('./sw.js').then(function(reg){
         console.log('Successfully registered service worker', reg);
     }).catch(function(err){
         console.warn('Error while registering service worker', err);
