5f2005a018c6831ca023f7dc9758dbe7578421db
JoelS <joelsweetman174@msn.com>
2017-07-27 01:07:09 +0800

    Moving Firebase code to fix issues in Microsoft Edge browser. Removing references to Push Messaging as they break in Safari and Edge.

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,17 @@
 // Register the service worker (if available).
 // Updating this to include push manager for updating the PWA
 // https://developers.google.com/web/fundamentals/getting-started/codelabs/push-notifications/
-if('serviceWorker' in navigator && 'PushManager' in window){
-    console.log('Service Worker and Push is supported');
+if('serviceWorker' in navigator /*&& 'PushManager' in window*/){
+    console.log('Service Worker is supported');
     navigator.serviceWorker.register('./service-worker.js').then(function(reg){
         console.log('Successfully registered service worker', reg);
     }).catch(function(err){
         console.warn('Error while registering service worker', err);
     });
-} else {
+} /*else {
     console.warn('Push messaging is not supported');
-    pushButton.textContent = 'Push Not Supported';
-}
+    //pushButton.textContent = 'Push Not Supported';
+}*/
 
 window.addEventListener('online', function(e) {
     // re-sync data with server
